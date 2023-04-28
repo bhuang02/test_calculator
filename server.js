f0fedc3df0c09f05e55e18e6890829f152ac68f8
bhuang02 <105864196+bhuang02@users.noreply.github.com>
2023-04-28 16:33:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 88;
+const port = process.env.PORT || 80;
 
 app.listen(port);
-console.log("Server running (88)");
+console.log("Server running (80)");
